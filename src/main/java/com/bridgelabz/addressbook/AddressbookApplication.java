ffc2d18f742525dce6c2ed9b42266ac4fed9ceb0
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-05-04 10:19:42 +0530

    Update Address Book Application wih Swagger UI

--- src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
@@ -41,7 +41,7 @@ public class AddressbookApplication {
 
 	}
 	private ApiInfo getApiInfo() {
-		springfox.documentation.service.Contact contact = new Contact("Demo Project API", "http://brigelbaz.com", "kdesale1211@gmail.com");
+		springfox.documentation.service.Contact contact = new Contact("Demo Project API", "http://brigelbaz.com", "almasbanu7866@gmail.com");
 		return new ApiInfoBuilder().title("Demo Service Swagger API")
 				.description("Demo Service Swagger API for Learning Swagger").version("0.0.1.SNAPSHOT")
 				.license("Apache 2.0").licenseUrl(" http://www.apache.org/licenses/LICENSE-2.0").contact(contact)
